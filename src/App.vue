a729fef575165da433c90911667ae0d30bc1d674
unknown <lourencovinicius00@gmail.com>
2023-10-19 22:45:47 -0300

    Criando pagina de login

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import Login from './components/Login.vue';
+
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">      
+      <Login/>
+    </div>
+
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+
+<template >
+
+  <div class="wrapper">
+    <form action="" class="login">
+      <div class="campos">
+        <div class="campo">
+          <label for="">Nome de Usuário</label>
+          <input type="text" name="" id="">
+        </div>
+        <div class="campo">
+          <label for="">Senha</label>
+          <input type="senha" name="" id="">
+        </div>
+        <button>Entrar</button>
+        <a href="">Não tenho uma conta, quero me registrar.</a>
+      </div>
+    </form>
+  </div>
+
+</template>
+
+<style scoped>  
+  .wrapper {
+    display: flex;    
+    align-items: center;
+    justify-content: center;
+  }
+
+  .login {    
+    width: 50vw;
+    height: 50vh;    
+    display: flex;
+    flex-direction: column;
+    align-items: center;  
+    justify-content: center;        
+    background-color: #191825;    
+  }
+
+  .campos {
+    width: 50%;
+    display: flex;
+    flex-direction: column;    
+    gap: 1rem; 
+  }
+
+  .campo {
+    display: flex;
+    flex-direction: column;    
+  }  
+
+  input {
+    width: 100%;
+  }
+
+  button {
+    width: 100%;
+  }
+
+</style>
+ 
